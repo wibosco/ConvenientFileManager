d23b86ab100641b27fe43afb802fb28640f04b4c
wibosco <wboles1986@gmail.com>
2016-01-17 16:56:18 +0000

    Updated created by value

--- Example/iOS Example/iOSExample/Model/ManagedObjects/CFEMedia+CoreDataProperties.m
+++ Example/iOS Example/iOSExample/Model/ManagedObjects/CFEMedia+CoreDataProperties.m
@@ -2,7 +2,7 @@
 //  CFEMedia+CoreDataProperties.m
 //  iOSExample
 //
-//  Created by Home on 17/01/2016.
+//  Created by Boles on 17/01/2016.
 //  Copyright © 2016 Boles. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
